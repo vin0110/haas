39a73306c4f0db0a741103822bce429d6c1e3155
Vince <leapinglemming@gmail.com>
2017-08-02 21:45:36 -0400

    removed provide option -- until needed

--- haascli/haas.py
+++ haascli/haas.py
@@ -8,8 +8,12 @@ import haascli
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('--debug/--no-debug', default=False)
 @click.option('--exec/--no-exec', default=True)
-@click.option('--provider', type=click.Choice(['aws', 'azure']), default='aws')
-@click.option('--config_dir', type=click.Path(exists=True, resolve_path=True), default=lambda: os.path.join(os.path.expanduser('~'), '.haas'), help="The haas configuration directory")
+# @click.option('--provider', type=click.Choice(['aws', 'azure']),
+#               default='aws')
+@click.option('--config_dir',
+              type=click.Path(exists=True, resolve_path=True),
+              default=lambda: os.path.join(os.path.expanduser('~'), '.haas'),
+              help="The haas configuration directory")
 @click.pass_context
 def cli(ctx, **kwargs):
     """This is a command line tool for HPCC-as-a-Service (HaaS)
