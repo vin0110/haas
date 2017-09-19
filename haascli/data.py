7831d22281eede72c777ef3e90a1c641156d436a
Chin-Jung Hsu <kmscout@gmail.com>
2017-09-19 11:30:09 -0400

    Change the script path.

--- haascli/data.py
+++ haascli/data.py
@@ -38,8 +38,7 @@ def save(ctx, stack_name, resource_name, checkpoint_name, regex):
     # didn't use RemoteCommand because I cannot make it work
     # base64 turns out to be an easy way to escape commands
     conf = HaasConfigurationManager().get(ctx.obj['config'])
-    cmd = "source /home/osr/haas/scripts/init.sh &&" +\
-          " $(nohup python /home/osr/haas/scripts/checkpoint.py --name {} service_{} save --regex '{}'" +\
+    cmd = "$(nohup python3 /opt/haas/checkpoint.py --name {} service_{} save --regex '{}'" +\
           " > {} 2>&1 &)".format(checkpoint_name, resource_name, regex, service_output)
     os.system("ssh -i {} -l {} {} 'echo {} | base64 -d | bash'".format(
         conf.get(HaasConfigurationKey.HAAS_SSH_KEY),
@@ -64,8 +63,7 @@ def restore(ctx, checkpoint_name, resource_name, stack_name, regex):
     service_output = "/tmp/haas_data.out"
 
     conf = HaasConfigurationManager().get(ctx.obj['config'])
-    cmd = "source /home/osr/haas/scripts/init.sh &&" +\
-          " $(nohup python /home/osr/haas/scripts/checkpoint.py --name {} service_{} restore --regex '{}'" +\
+    cmd = "$(nohup python3 /opt/haas/scripts/checkpoint.py --name {} service_{} restore --regex '{}'" +\
           " > {} 2>&1 &)".format(checkpoint_name, resource_name, regex, service_output)
     os.system("ssh {} 'echo {} | base64 -d | bash'".format(
         conf.get(HaasConfigurationKey.HAAS_SSH_KEY),
@@ -86,7 +84,7 @@ def progress(ctx, stack_name):
 
     conf = HaasConfigurationManager().get(ctx.obj['config'])
     cmd = RemoteCommand(topology.get_master_ip(),
-                        'source ~/haas/scripts/init.sh; python /home/osr/haas/scripts/checkpoint.py --name {} available; echo $?',
+                        'python3 /opt/haas/scripts/checkpoint.py --name {} available; echo $?',
                         identity_file=conf.get(HaasConfigurationKey.HAAS_SSH_KEY),
                         ssh_user=conf.get(HaasConfigurationKey.HAAS_SSH_USER),
                         capture=True)
